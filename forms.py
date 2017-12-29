0ceb84e8462db8a5f9892351ca9386b43dfbd5c9
jiaschien <leslie07168@hotmail.com>
2017-12-29 17:39:07 +0800

    添加评论功能

--- forms.py
+++ forms.py
@@ -1,1 +1,8 @@
-from django import formsclass EmailPostForm(forms.Form):    name = forms.CharField(max_length=25)    email = forms.EmailField()    to = forms.EmailField()    comments = forms.CharField(required=False, widget=forms.Textarea)
+from django import forms
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
